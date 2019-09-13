aba2e870e3c707c0fbceac3b0652546b7796dcb5
Matthew J. Martin <matthew.mar10@gmail.com>
2019-09-13 21:01:20 +0700

    export url utils

--- src/protractor-mink.js
+++ src/protractor-mink.js
@@ -6,6 +6,7 @@ const { by } = require('protractor');
 const steps = require('./step-definitions');
 const locators = require('./utils/by');
 const elementFinders = require('./utils/element');
+const url = require('./utils/url');
 
 const names = Object.keys(locators);
 names.forEach((name) => {
@@ -29,4 +30,5 @@ module.exports = {
   steps,
   by: locators,
   element: elementFinders,
+  url,
 };
